141a650fc19980e826c403080f02a06ffda1e343
Joseph Shitandi Otaaba <joseph.otaaba@mgas.ke>
2024-02-22 15:01:00 +0300

    Added class Order

--- shop/models.py
+++ shop/models.py
@@ -57,3 +57,18 @@ class Sub_Category(models.Model):
     def delete_sub_category(self):
         self.delete()
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(_('date of order'), auto_now_add=True)
+    product_id = models.ForeignKey(
+        "Product", on_delete=models.CASCADE, related_name='order')
+    delivered = models.BooleanField()
+
+    # def __str__(self):
+    #     return self.date
+
+    def save_order(self):
+        self.save()
+
+    def delete_order(self):
+        self.delete()
