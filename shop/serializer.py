4307ca89409e160e18c82de14af0ed8c4154eee9
Joseph Shitandi Otaaba <joseph.otaaba@mgas.ke>
2024-02-22 16:50:33 +0300

    Added UserSignupSerializer for creating creating signup endpoints

--- shop/serializer.py
+++ shop/serializer.py
@@ -10,4 +10,18 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ('id','username','user','avatar', 'address', 'phone_number','region')
 
-        
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','user_type','email','first_name','last_name','password']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSignupSerializer, self).create(validated_data)
+
+
+
+
+
