1afb2a9a0a06533dfc4d358f1b3094c077afb6cb
Joseph Shitandi Otaaba <joseph.otaaba@mgas.ke>
2024-02-22 17:20:47 +0300

    Added IsAdmin for authorizing only Admins the Admin rights

--- shop/permissions.py
+++ shop/permissions.py
@@ -7,3 +7,11 @@ class IsAdminOrReadOnly(BasePermission):
             return True
         else:
             return request.user.is_staff
+        
+class IsAdmin(BasePermission): 
+    def has_permission(self, request, view):
+        admin = request.user.user_type ==3
+        return bool(
+             super().has_permission(request, view)
+             and (admin)
+            )
