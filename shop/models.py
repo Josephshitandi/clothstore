890093048aeed35aa617c07d1ca998280910f17c
Joseph Shitandi Otaaba <joseph.otaaba@mgas.ke>
2024-02-22 14:48:38 +0300

    Added class Product in models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Product(models.Model):
+    item_name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image')
+    image2 = CloudinaryField('image_2', blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    color = models.CharField(max_length=100, blank=True, null=True)
+    shipped_from = models.ForeignKey("Shop", on_delete=models.CASCADE, related_name='shop')
+    size = models.CharField(max_length=100, blank=True)
+    brand = models.CharField(max_length=100, blank=True)
+    sub_category = models.ForeignKey(
+        "Sub_Category", on_delete=models.CASCADE, related_name='sub_categ')
+
+    def __str__(self):
+        return self.item_name
+
+    def save_product(self):
+        self.save()
+
+    def delete_product(self):
+        self.delete()
